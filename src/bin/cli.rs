9d8cab6811374ebb69ed2fd7a13bff1950ee81cd
Richard Lyon <richlyon@mac.com>
2023-05-29 06:52:51 +0100

    start Accounts command

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,11 +1,6 @@
 //! `starling` ia a command line application for processing Starling transactions.
 //!
 //! Transactionsa are stored in a database. Reports can be produced in [ledger](https://ledger-cli.org/features.html) format.
-//! seaql, seaorm, sqlx
-
-// mod db;
-// mod entities;
-// mod starling;
 
 use clap::{Parser, Subcommand};
 use std::env;
@@ -14,16 +9,19 @@ use money::db;
 use money::starling::client::{StarlingApiClient, StarlingClient};
 
 /// Command line arguments
-#[derive(Parser, Debug, Clone)]
+#[derive(Parser)]
 #[clap(about, version, author)]
-struct Args {
+struct Cli {
     #[clap(subcommand)]
-    command: Command,
+    command: Commands,
 }
 
 /// Commands
-#[derive(Clone, Debug, Subcommand)]
-enum Command {
+#[derive(Subcommand)]
+enum Commands {
+    /// Load accounts to the database
+    Accounts,
+
     /// Account balances
     Balances,
 
@@ -39,17 +37,20 @@ enum Command {
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
     dotenvy::dotenv().ok();
-    let args = Args::parse();
+
+    let cli = Cli::parse();
 
     let personal_token =
         env::var("PERSONAL_TOKEN").expect("PERSONAL_TOKEN is not set in .env file");
     let client = StarlingApiClient::new(personal_token);
     // let client = StarlingMockClient::new();
 
-    match args.command {
-        Command::Balances => todo!(),
+    match cli.command {
+        Commands::Accounts => todo!(),
 
-        Command::Transactions { days } => {
+        Commands::Balances => todo!(),
+
+        Commands::Transactions { days } => {
             if let Some(account) = client.accounts().await.iter().next() {
                 db::service::insert_or_update(&client, account, days).await;
             }
